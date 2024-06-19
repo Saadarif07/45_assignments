830ddc5a4e362a66185510f951f03d739e4243a2
Saadarif07 <160487578+Saadarif07@users.noreply.github.com>
2024-06-19 13:54:32 +0500

    updated

--- main.js
+++ main.js
@@ -8,10 +8,10 @@ console.log(personName.toLowerCase());
 console.log(personName.toUpperCase());
 console.log(personName.replace(/\b\w/g, function (char) { return char.toUpperCase(); }));
 // Question 4 solution:
-console.log("Cristiano Ronaldo once said,\"Your love makes me strong,your hate makes me unstoppable.\"");
+console.log('Cristiano Ronaldo once said,"Your love makes me strong,your hate makes me unstoppable."');
 // Question 5 solution:
 var famous_person = "Cristiano Ronaldo";
-var message = "once said, \"Your love makes me strong,your hate makes me unstoppable\"";
+var message = 'once said, "Your love makes me strong,your hate makes me unstoppable"';
 console.log(famous_person, message);
 // Question 6 solution:
 var whiteSpaceName = "\n\t Saad Arif \t\n";
@@ -29,7 +29,7 @@ var messages = "My favorite number is : ";
 console.log(messages, favNumber);
 // Question 10 solution:
 // Add comments to any program
-console.log(6 + 2); // Addition 
+console.log(6 + 2); // Addition
 console.log(10 - 2); // Subtraction
 console.log(2 * 4); // Multiplication
 console.log(16 / 2); // Division
@@ -43,7 +43,28 @@ friendsName1.forEach(function (name) {
 });
 // Question 13 solution:
 var transportationModes = ["Bike", "Car", "Bus"];
-transportationModes.forEach(function (mode) { return console.log("I would like to own a ".concat(mode, " one day.")); });
+transportationModes.forEach(function (mode) {
+    return console.log("I would like to own a ".concat(mode, " one day."));
+});
 //  Question 14 solution:
-var guestList = ["Farhan", "Munnaza", "Faiq"];
-guestList.forEach(function (guest) { return console.log("Salam ".concat(guest, ", I would like to invite you to dinner, it would be a great pleasure if you came.")); });
+var guestList = ["Farhan", "Munnaza", "Faiq", "Bilal"];
+guestList.forEach(function (guest) {
+    return console.log("Salam ".concat(guest, ", I would like to invite you to dinner, it would be a great pleasure if you came."));
+});
+// Question 15 solution:
+var guestList2 = ["Farhan", "Munnaza", "Faiq", "Bilal"];
+var cantMakeIt = guestList2[2];
+console.log(cantMakeIt, "cannot come");
+guestList2.splice(2, 2, "Taha");
+guestList2.forEach(function (guests) {
+    return console.log("Salam ".concat(guests, ", I would like to invite you to dinner, it would be a great pleasure if you came."));
+});
+//  Question 16 solution:
+console.log("\nGood News!,I found a bigger dinner table.");
+guestList2.unshift("Hammad"); // Add to beggining
+guestList2.push("Sajjad"); // Add to end
+// Adding middle value
+var middleIndex = Math.floor(guestList2.length / 2);
+guestList2.splice(middleIndex, 0, "Burhan");
+console.log("Updated list of guests");
+guestList2.forEach(function (oneguest) { return console.log("Salam ".concat(oneguest, ", I would like to invite you to dinner, it would be a great pleasure if you came.")); });
--- main.ts
+++ main.ts
@@ -1,5 +1,5 @@
 // Question 2 soltion:
-let personName: string= "ERIC";
+let personName: string = "ERIC";
 console.log(`Hello ${personName}, Would you like to learn some Python today?`);
 
 // Question 3 solution:
@@ -10,17 +10,20 @@ console.log(personName.toLowerCase());
 
 console.log(personName.toUpperCase());
 
-console.log(personName.replace(/\b\w/g, (char) => char.toUpperCase()))
+console.log(personName.replace(/\b\w/g, (char) => char.toUpperCase()));
 
 // Question 4 solution:
-console.log("Cristiano Ronaldo once said,\"Your love makes me strong,your hate makes me unstoppable.\"")
+console.log(
+  'Cristiano Ronaldo once said,"Your love makes me strong,your hate makes me unstoppable."'
+);
 
 // Question 5 solution:
 let famous_person = "Cristiano Ronaldo";
 
-let message = "once said, \"Your love makes me strong,your hate makes me unstoppable\""
+let message =
+  'once said, "Your love makes me strong,your hate makes me unstoppable"';
 
-console.log(famous_person, message)
+console.log(famous_person, message);
 
 // Question 6 solution:
 let whiteSpaceName = "\n\t Saad Arif \t\n";
@@ -30,13 +33,13 @@ let withoutWhiteSpace = whiteSpaceName.trim();
 console.log(withoutWhiteSpace);
 
 // Question 7&8 soulution:
-console.log(6+2);
+console.log(6 + 2);
 
-console.log(10-2);
+console.log(10 - 2);
 
-console.log(2*4);
+console.log(2 * 4);
 
-console.log(16/2);
+console.log(16 / 2);
 
 // Question 9 solution:
 let favNumber = 7;
@@ -45,13 +48,13 @@ console.log(messages, favNumber);
 
 // Question 10 solution:
 // Add comments to any program
-console.log(6+2); // Addition 
+console.log(6 + 2); // Addition
 
-console.log(10-2); // Subtraction
+console.log(10 - 2); // Subtraction
 
-console.log(2*4); // Multiplication
+console.log(2 * 4); // Multiplication
 
-console.log(16/2); // Division
+console.log(16 / 2); // Division
 
 // Question 11 solution:
 let friendsName = ["Ashar", "Hamza", "Daniyal"];
@@ -59,14 +62,44 @@ console.log(friendsName);
 
 // Question 12 solution:
 let friendsName1 = ["Ashar", "Hamza", "Daniyal"];
-friendsName1.forEach(name => {
-    console.log(`Hello, ${name} How are you?`);
+friendsName1.forEach((name) => {
+  console.log(`Hello, ${name} How are you?`);
 });
 
 // Question 13 solution:
 let transportationModes = ["Bike", "Car", "Bus"];
-transportationModes.forEach(mode => console.log(`I would like to own a ${mode} one day.`));
+transportationModes.forEach((mode) =>
+  console.log(`I would like to own a ${mode} one day.`)
+);
 
 //  Question 14 solution:
-let guestList = ["Farhan", "Munnaza", "Faiq"];
-guestList.forEach(guest => console.log(`Salam ${guest}, I would like to invite you to dinner, it would be a great pleasure if you came.`));
+let guestList = ["Farhan", "Munnaza", "Faiq","Bilal"];
+guestList.forEach((guest) =>
+  console.log(
+    `Salam ${guest}, I would like to invite you to dinner, it would be a great pleasure if you came.`
+  )
+);
+
+// Question 15 solution:
+let guestList2 = ["Farhan", "Munnaza", "Faiq","Bilal"];
+let cantMakeIt = guestList2[2];
+console.log(cantMakeIt, "cannot come");
+
+guestList2.splice(2, 2, "Taha");
+
+guestList2.forEach((guests) =>
+  console.log(
+    `Salam ${guests}, I would like to invite you to dinner, it would be a great pleasure if you came.`
+  )
+);
+//  Question 16 solution:
+console.log("\nGood News!,I found a bigger dinner table.");
+guestList2.unshift("Hammad");// Add to beggining
+guestList2.push("Sajjad");// Add to end
+// Adding middle value
+let middleIndex: number = Math.floor(guestList2.length / 2);
+guestList2.splice(middleIndex,0, "Burhan");
+
+console.log("Updated list of guests");
+
+guestList2.forEach(oneguest => console.log(`Salam ${oneguest}, I would like to invite you to dinner, it would be a great pleasure if you came.`));
